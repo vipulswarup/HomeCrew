37b812a7b293b86b83fa675be963aea9a933b308
Vipul Swarup <vipul.swarup@eisenvault.com>
2025-03-07 16:29:31 +0530

    Initial Commit

---
+++ HomeCrewTests/HomeCrewTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCrewTests.swift
+//  HomeCrewTests
+//
+//  Created by Vipul Swarup on 07/03/25.
+//
+
+import Testing
+@testable import HomeCrew
+
+struct HomeCrewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ HomeCrewUITests/HomeCrewUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HomeCrewUITestsLaunchTests.swift
+//  HomeCrewUITests
+//
+//  Created by Vipul Swarup on 07/03/25.
+//
+
+import XCTest
+
+final class HomeCrewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
